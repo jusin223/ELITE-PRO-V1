7500bab9a96365d2dfe55f4c7961067178be1ce4
jusin223 <tinashe2032@gmail.com>
2025-04-25 19:14:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,16 +3,16 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "263714257867"
+global.ownername = "MR Matems"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
-global.location = "Nigeria, Port Harcourt"
+global.location = "zimbabwe, Marondera"
 
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
-global.botname = 'ELITE-PRO-V1' //name of the bot
+global.ownernumber = '263714257867'  //creator number
+global.ownername = 'MR Matems' //owner name
+global.botname = 'MATEMS-PRO-V1' //name of the bot
 
 
 //custom prefix
@@ -20,17 +20,17 @@ global.prefa = ['','!','.','#','&']
 
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
-global.antilink = false // default off
+global.antilink = true // default off
 
 
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
-global.welcome = true // or false to disable
+global.welcome = false // or false to disable
 
 //sticker details
 global.packname = 'Sticker By'
